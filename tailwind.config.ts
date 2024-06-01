57f248e4886f82858223db1a860f458c957d42f1
Franco Lorenzo <franco14lorenzo@gmail.com>
2024-06-01 00:12:51 -0300

    chore: update dependencies and configuration files

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,30 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
+      colors: {
+        pearl: {
+          '50': '#faf3eb',
+          '100': '#f7ebdd',
+          '200': '#edd5bb',
+          '300': '#e2b78f',
+          '400': '#d59362',
+          '500': '#cc7843',
+          '600': '#be6338',
+          '700': '#9e4e30',
+          '800': '#7f402d',
+          '900': '#673627',
+          '950': '#371a13'
+        }
+      }
+    }
   },
-  plugins: [],
-};
-export default config;
+  plugins: []
+}
+export default config
