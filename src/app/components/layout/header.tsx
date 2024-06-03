86276f950f0dc866b7fc3780f308fd22acf54710
Franco Lorenzo <franco14lorenzo@gmail.com>
2024-06-02 22:15:46 -0300

    fix: improve accessibility in header component

--- src/app/components/layout/header.tsx
+++ src/app/components/layout/header.tsx
@@ -5,7 +5,10 @@ import Link from 'next/link'
 const Header = () => {
   return (
     <header className="fixed z-10 flex w-full max-w-screen-xl items-center justify-between overflow-hidden rounded-xl rounded-t-none bg-pearl-50 p-2 shadow">
-      <div className="flex w-12 justify-end md:hidden md:w-[152px]">
+      <div
+        className="flex w-12 justify-end md:hidden md:w-[152px]"
+        aria-label="menu"
+      >
         <button className="grid size-12 place-content-center rounded-lg hover:bg-pearl-100">
           <Menu className="size-8 stroke-black" />
         </button>
@@ -20,6 +23,7 @@ const Header = () => {
       <Link
         className="flex items-center space-x-2 p-0 hover:opacity-70 md:pl-2"
         href="/"
+        aria-label="home"
       >
         <Grape className="size-8" />
         <p className="hidden font-kalnia text-2xl font-medium md:block">
@@ -40,7 +44,10 @@ const Header = () => {
       </nav>
 
       <div className="flex w-12 justify-end md:w-[113px]">
-        <button className="grid size-12 place-content-center rounded-lg hover:opacity-70">
+        <button
+          className="grid size-12 place-content-center rounded-lg hover:opacity-70"
+          aria-label="shopping bag"
+        >
           <ShoppingBag className="size-8 fill-black stroke-pearl-50" />
         </button>
       </div>
