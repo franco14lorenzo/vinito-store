35a26421538a122c3ea70a9cd816fb75316e56f5
Franco Lorenzo <franco14lorenzo@gmail.com>
2024-06-06 23:50:37 -0300

    feat: add notification badge to cart button

--- src/app/components/layout/header/cart/index.tsx
+++ src/app/components/layout/header/cart/index.tsx
@@ -4,10 +4,13 @@ const Cart = () => {
   return (
     <div className="flex w-12 justify-end md:w-[113px]">
       <button
-        className="grid size-12 place-content-center rounded-lg hover:opacity-70"
+        className="relative grid size-12 place-content-center rounded-lg hover:opacity-70"
         aria-label="shopping bag"
       >
         <ShoppingBag className="size-8 fill-black stroke-pearl-50" />
+        <span className="absolute right-0 top-0 flex h-5 w-5 items-center justify-center rounded-full border border-pearl-50 bg-red-500 text-xs font-medium leading-5 text-white">
+          10
+        </span>
       </button>
     </div>
   )
