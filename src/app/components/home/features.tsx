ef03d17e33f5ec7fcd28c64575536ed9056bca45
Franco Lorenzo <franco14lorenzo@gmail.com>
2024-06-21 15:53:44 -0300

    style: update home features layout for better alignment

--- src/app/components/home/features.tsx
+++ src/app/components/home/features.tsx
@@ -38,7 +38,7 @@ export const Feature = ({
   description: string
 }>) => {
   return (
-    <article className="flex flex-col items-center justify-start gap-2 text-balance rounded-lg bg-gradient-to-b from-pearl-100 to-pearl-50 p-6 text-left">
+    <article className="flex flex-col items-start justify-start gap-2 text-balance rounded-lg bg-gradient-to-b from-pearl-100 to-pearl-50 p-6 text-left">
       {icon && icon}
       <h2 className="text-base font-bold">{title}</h2>
       <p className="text-sm">{description}</p>
--- src/app/components/home/hero.tsx
+++ src/app/components/home/hero.tsx
@@ -7,13 +7,13 @@ const Hero = () => {
     <section className="flex flex-col items-center justify-center px-4 text-center">
       <div className="flex h-[400px] w-full flex-col items-center justify-center overflow-hidden rounded-lg">
         <p className="mb-4 text-sm font-medium uppercase text-zinc-950">
-          Live the best experience
+          From Mendoza to the world
         </p>
         <h1 className="font-kalnia text-6xl font-bold text-zinc-950">
           Taste Vinito
         </h1>
         <p className="mb-4 mt-3 text-2xl font-medium text-zinc-950">
-          From Mendoza to the world
+          Live the best experience
         </p>
         <Link
           className="mt-4 max-w-fit rounded-full border border-white bg-black px-4 py-2 font-medium text-white hover:bg-opacity-80"
