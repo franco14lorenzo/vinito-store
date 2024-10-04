b070a389a7de80d1bed9f68dd2b3ec9f644cb41a
Franco Lorenzo <franco14lorenzo@gmail.com>
2024-10-04 17:16:14 -0300

    fix: add slug column to tastings table and update its default value

--- supabase/migrations/20241004162629_update_wines_and_tastings_tables.sql
+++ supabase/migrations/20241004162629_update_wines_and_tastings_tables.sql
@@ -1,8 +1,15 @@
 ALTER TABLE "public"."tastings"
 ADD COLUMN "stock" INT DEFAULT 0 NOT NULL,
 ADD COLUMN "image" TEXT,
-ADD COLUMN "slug" TEXT NOT NULL,
+ADD COLUMN "slug" TEXT,
 ADD COLUMN "long_description" TEXT;
+
+UPDATE "public"."tastings"
+SET "slug" = 'default-slug'
+WHERE "slug" IS NULL;
+
+ALTER TABLE "public"."tastings"
+ALTER COLUMN "slug" SET NOT NULL;
 
 ALTER TABLE "public"."tastings"
 RENAME COLUMN "description" TO "short_description";
