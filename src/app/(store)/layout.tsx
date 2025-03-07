75f8b3f47e479d86ef13f906ccd7095da5aa1c85
Franco Lorenzo <franco14lorenzo@gmail.com>
2025-03-07 13:01:26 -0300

    fix: comment out Header component in StoreLayout

--- src/app/(store)/layout.tsx
+++ src/app/(store)/layout.tsx
@@ -1,6 +1,6 @@
 import { ReactNode } from 'react'
 
-import { Footer, Header } from '@/app/(store)/components/layout'
+import { Footer /* Header */ } from '@/app/(store)/components/layout'
 import { CartProvider } from '@/app/(store)/contexts/cart'
 import { DialogProvider } from '@/app/contexts/dialogs'
 
@@ -13,7 +13,7 @@ export default function StoreLayout({
     <>
       <CartProvider>
         <DialogProvider>
-          <Header />
+          {/*    <Header /> */}
           <main className="mt-16 flex min-h-screen w-full max-w-(--breakpoint-xl) flex-col items-center justify-start p-2">
             {children}
           </main>
