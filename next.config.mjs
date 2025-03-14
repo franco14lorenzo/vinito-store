2bdc40ed72bfd98c63ae7fb23c03978d49e5d9dd
Franco Lorenzo <franco14lorenzo@gmail.com>
2025-03-14 16:58:38 -0300

    feat: add PostHog rewrites for asset ingestion and update PostHog initialization configuration

--- next.config.mjs
+++ next.config.mjs
@@ -12,7 +12,25 @@ const nextConfig = {
         hostname: '127.0.0.1'
       }
     ]
-  }
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/ingest/static/:path*',
+        destination: 'https://us-assets.i.posthog.com/static/:path*'
+      },
+      {
+        source: '/ingest/:path*',
+        destination: 'https://us.i.posthog.com/:path*'
+      },
+      {
+        source: '/ingest/decide',
+        destination: 'https://us.i.posthog.com/decide'
+      }
+    ]
+  },
+  // This is required to support PostHog trailing slash API requests
+  skipTrailingSlashRedirect: true
 }
 
 export default withSentryConfig(nextConfig, {
